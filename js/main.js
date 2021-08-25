074666ae8883f6f1900115b0b9845c77a6267ee5
AsadbekMakhmudjonov <asadbek.mah.@gmail.com>
2021-08-25 14:47:58 +0500

    finished

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var elInput = document.querySelector(".js-input");
+var elForm = document.querySelector(".form");
+var elOutput = document.querySelector(".js-output");
+
+
+
+var calculateFizBuzz = function () {
+  var numberInput = +elInput.value;
+  if (numberInput == 0) {
+    elOutput.textContent = "";
+  }
+  else if ( numberInput % 3 == 0 && numberInput % 5 == 0){
+    elOutput.textContent = "FizzBuzz";
+  } else if(numberInput % 3 == 0){
+    elOutput.textContent = "Fizz"
+  } else if(numberInput % 5 == 0 ){
+    elOutput.textContent = "Buzz";
+  } else{
+    elOutput.textContent = numberInput;
+  }
+}
+
+elForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  calculateFizBuzz();
+  // elOutput.textContent = "";
+
+})
+
